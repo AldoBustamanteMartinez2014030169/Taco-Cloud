1d183864b78abc44c7be6249739e789e2ccf6126
Aldo Bustamante <aldo.bustamanteipn@hotmail.com>
2023-12-22 13:52:31 -0600

    Validacion en FORM

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import java.util.List;
+import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class Taco {
+	@NotBlank(message = "Name must not be blank")
+	private String name;
+	@NotBlank(message = "You must choose at least 1 ingredient")
+	private List<String> ingredients;
+}
